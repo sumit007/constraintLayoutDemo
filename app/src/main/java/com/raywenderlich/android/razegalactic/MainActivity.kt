de5b053bea6b952ca98a4cb1bddc18e4674095d3
sumit007 <sumit@nestaway.com>
2019-02-20 16:17:05 +0530

    completed the courses

--- app/src/main/java/com/raywenderlich/android/razegalactic/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/razegalactic/MainActivity.kt
@@ -30,14 +30,24 @@
 
 package com.raywenderlich.android.razegalactic
 
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import android.transition.TransitionManager
+import android.view.animation.LinearInterpolator
+import kotlinx.android.synthetic.main.keyframe1.*
 
 /**
  * Main Screen
  */
 class MainActivity : AppCompatActivity() {
+
+  private val constraintSet1 = ConstraintSet()
+  private val constraintSet2 = ConstraintSet()
+
+  private var isOffscreen = true
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -45,6 +55,43 @@ class MainActivity : AppCompatActivity() {
 
     switch1.setOnCheckedChangeListener { _, isChecked ->
       switch1.setText(if (isChecked) R.string.round_trip else R.string.one_way)
+    }
+
+    constraintSet1.clone(constraintLayout) //1
+    constraintSet2.clone(this, R.layout.activity_main) //2
+
+    departButton.setOnClickListener { //3
+      /*//apply the transition
+      TransitionManager.beginDelayedTransition(constraintLayout) //4
+      val constraint = if (!isOffscreen) constraintSet1 else constraintSet2
+      isOffscreen = !isOffscreen
+      constraint.applyTo(constraintLayout) //5*/
+
+      //1
+      val layoutParams = rocketIcon.layoutParams as ConstraintLayout.LayoutParams
+      val startAngle = layoutParams.circleAngle
+      val endAngle = startAngle + (if (switch1.isChecked) 360 else 180)
+
+      //2
+      val anim = ValueAnimator.ofFloat(startAngle, endAngle)
+      anim.addUpdateListener { valueAnimator ->
+
+        //3
+        val animatedValue = valueAnimator.animatedValue as Float
+        val layoutParams = rocketIcon.layoutParams as ConstraintLayout.LayoutParams
+        layoutParams.circleAngle = animatedValue
+        rocketIcon.layoutParams = layoutParams
+
+        //4
+        rocketIcon.rotation = (animatedValue % 360 - 270)
+      }
+      //5
+      anim.duration = if (switch1.isChecked) 2000 else 1000
+
+      //6
+      anim.interpolator = LinearInterpolator()
+      anim.start()
+
     }
   }
 }
